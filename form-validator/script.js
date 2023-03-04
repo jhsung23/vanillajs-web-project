e44f007fcb4922b44739a67cf13ffb9bd3bfce44
sungjihyun <jhsung23@gmail.com>
2023-03-04 23:46:59 +0900

    feat: 회원가입 인풋 유효성 검사

---
+++ form-validator/script.js
@@ -0,0 +1,64 @@
+// querySelector로 해당 id의 DOM을 가져온다.
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  // input의 부모 요소 가져오기
+  const formControl = input.parentElement; // div
+  // formControl의 클래스 이름 변경
+  formControl.className = 'form-control error';
+  // formControl 내부에서 small이라는 태그 가져오기
+  const small = formControl.querySelector('small');
+  // small 태그의 텍스트 변경
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  // input의 부모 요소 가져오기
+  const formControl = input.parentElement; // div
+  // formControl의 클래스 이름 변경
+  formControl.className = 'form-control success';
+}
+
+function isValidEmail(email) {
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return regex.test(String(email).toLowerCase());
+}
+
+// Event listeners
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // submit시 자동 새로고침을 막음
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Confirm password is required');
+  } else {
+    showSuccess(password2);
+  }
+});
