81cc6d14bd48dfbf046f8701caab20298a739959
sungjihyun <jhsung23@gmail.com>
2023-03-05 16:47:25 +0900

    feat: 영화 선택 기능

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,6 +4,12 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 
+let ticketPrice = +movieSelect.value;
+
+movieSelect.addEventListener('change', (event) => {
+  ticketPrice = +event.target.value;
+});
+
 container.addEventListener('click', (event) => {
   if (event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
     event.target.classList.toggle('selected');
