abac46e9d9af439f44ccb99f2a954dffb1b6da09
sungjihyun <jhsung23@gmail.com>
2023-03-05 16:48:41 +0900

    feat: 선택한 좌석 개수와 가격 표시 기능

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -6,12 +6,24 @@ const total = document.getElementById('total');
 
 let ticketPrice = +movieSelect.value;
 
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .selected');
+  const selectedSeatCount = +selectedSeats.length;
+
+  count.textContent = selectedSeatCount;
+  total.textContent = selectedSeatCount * ticketPrice;
+}
+
 movieSelect.addEventListener('change', (event) => {
   ticketPrice = +event.target.value;
+
+  updateSelectedCount();
 });
 
 container.addEventListener('click', (event) => {
   if (event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
     event.target.classList.toggle('selected');
+
+    updateSelectedCount();
   }
 });
