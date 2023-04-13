913f54d7942bf09c34a69aa201ae2bc517ff383f
sungjihyun <jhsung23@gmail.com>
2023-04-13 12:21:35 +0900

    feat: 내비게이션 바 토글 버튼과 모달 보이기/숨기기 버튼 기능 구현

---
+++ modal-menu-slider/script.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+
+// Show modal
+open.addEventListener('click', () => {
+  document.body.classList.remove('show-nav');
+  modal.classList.add('show-modal');
+});
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', (e) =>
+  e.target === modal ? modal.classList.remove('show-modal') : undefined
+);
